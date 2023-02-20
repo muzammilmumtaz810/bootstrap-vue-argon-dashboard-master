d75b53c670d3dcc3aa54068e74c943951ee0200e
muzammilmumtaz810 <121790589+muzammilmumtaz810@users.noreply.github.com>
2023-02-20 15:13:27 +0500

    timer complete
    checkin.vue complete

--- src/components/CheckIn.vue
+++ src/components/CheckIn.vue
@@ -1,26 +1,15 @@
 <template>
   <div class="d-flex">
-    <!-- <p class="timer">Check-in: {{ checkin }}</p>
-    <p class="timer">Check-out: {{ checkout }}</p> -->
-    <!-- <p class="timer" v-if="checkin && checkout">
-      Time worked: {{ totalHours }} : {{ totalMinutes }} :
-      {{ totalSeconds }}
-    </p> -->
-    <p class="timer popup" v-if="showDiv">
-      {{ hours }} : {{ minutes }} : {{ seconds }}
-    </p>
     <button
       class="new btn base-button btn-primary btn-sm"
-      @mouseover="showDiv = true"
-      @mouseleave="showDiv = false"
+      v-b-popover.hover.bottom
+      :title="TimeFormate(runningTime)"
       @click="toggleCheckInOut"
     >
       {{ buttonText }}
     </button>
-
     <p class="timer">
-      {{ totalTimeInHours }} : {{ totalTimeInMinutes }} :
-      {{ totalTimeInSeconds }}
+      {{ TimeFormate(total_day_seconds + runningTime) }}
     </p>
   </div>
 </template>
@@ -33,30 +22,28 @@ export default {
 
   data() {
     return {
-      totalTime: 0,
-      showDiv: false,
+      total_day_seconds: 0,
       checkedIn: false,
-      totalTimeInHours: 0,
-      totalTimeInMinutes: 0,
-      totalTimeInSeconds: 0,
-      showdaytime: false,
       checkin: null,
       checkout: null,
       runningTime: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-
-      totalHours: 0,
-      totalMinutes: 0,
-      totalSeconds: 0,
       isRunningTimePaused: false,
       intervalId: null,
-      stoppedTime: 0,
       employe_id: ""
     };
   },
   methods: {
+    TimeFormate(second) {
+      const hours = Math.floor(second / 3600);
+      const remainingSecondsAfterHours = second % 3600;
+      const minutes = Math.floor(remainingSecondsAfterHours / 60);
+      const seconds = Math.floor(remainingSecondsAfterHours % 60);
+      return `${hours
+        .toString()
+        .padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    },
     toggleCheckInOut() {
       if (this.checkedIn) {
         this.endCheckout();
@@ -71,10 +58,7 @@ export default {
         if (!this.isRunningTimePaused) {
           let checkinTime = new Date(this.checkin).getTime();
           let currentTime = new Date().getTime();
-          this.runningTime = (currentTime - checkinTime) / 1000;
-          this.hours = Math.floor(this.runningTime / 3600);
-          this.minutes = Math.floor((this.runningTime % 3600) / 60);
-          this.seconds = Math.floor(this.runningTime % 60);
+          this.runningTime = Math.floor((currentTime - checkinTime) / 1000);
         }
       }, 1000);
       axios.post("http://localhost:3000/employetime", {
@@ -90,9 +74,6 @@ export default {
         (new Date(this.checkout).getTime() - new Date(this.checkin).getTime()) /
           1000 -
         this.stoppedTime;
-      this.totalHours = Math.floor(this.totalTime / 3600);
-      this.totalMinutes = Math.floor((this.totalTime % 3600) / 60);
-      this.totalSeconds = Math.floor(this.totalTime % 60);
 
       this.timesend();
     },
@@ -106,7 +87,6 @@ export default {
       );
       if (updatedObject) {
         updatedObject.checkout = this.checkout;
-        updatedObject.totalwork = this.totalTime;
 
         await axios.put(
           `http://localhost:3000/employetime/${updatedObject.id}`,
@@ -135,9 +115,6 @@ export default {
             let currentTime = new Date().getTime();
             let checkintime = new Date(this.checkin).getTime();
             this.runningTime = (currentTime - checkintime) / 1000;
-            this.hours = Math.floor(this.runningTime / 3600);
-            this.minutes = Math.floor((this.runningTime % 3600) / 60);
-            this.seconds = Math.floor(this.runningTime % 60);
           }
         }, 1000);
       }
@@ -154,39 +131,19 @@ export default {
         for (let i = 0; i < filteredData.length; i++) {
           const currentItem = filteredData[i];
 
-          if (currentItem.hasOwnProperty("checkin")) {
-            let checkout = filteredData.every(currentItem =>
-              currentItem.hasOwnProperty("checkout")
-            );
+          const newdate = new Date();
+          const checkinDate = new Date(currentItem.checkin);
 
-            if (checkout) {
-              const newdate = new Date();
-              const checkinDate = new Date(currentItem.checkin);
+          const checkoutDate = new Date(currentItem.checkout);
 
-              const checkoutDate = new Date(currentItem.checkout);
+          if (newdate.getDate() === checkoutDate.getDate()) {
+            const timeDifference =
+              checkoutDate.getTime() - checkinDate.getTime();
 
-              if (newdate.getDate() === checkoutDate.getDate()) {
-                const timeDifference =
-                  checkoutDate.getTime() - checkinDate.getTime();
-
-                totalTime += timeDifference;
-                this.totalTime = totalTime;
-              }
-            }
-          }
-          if (
-            !currentItem.hasOwnProperty("checkout") &&
-            currentItem.hasOwnProperty("checkin")
-          ) {
-            this.totalTime = this.totalTime + 1000;
+            totalTime += timeDifference;
+            this.total_day_seconds = Math.floor(totalTime / 1000);
           }
         }
-
-        const totalTimeInSeconds = Math.round(this.totalTime / 1000);
-
-        this.totalTimeInHours = Math.floor(totalTimeInSeconds / 3600);
-        this.totalTimeInMinutes = Math.floor((totalTimeInSeconds % 3600) / 60);
-        this.totalTimeInSeconds = totalTimeInSeconds % 60;
       } catch (error) {
         console.error(error);
       }
@@ -197,16 +154,14 @@ export default {
       return this.checkedIn ? "CHECK OUT" : "CHECK IN";
     }
   },
+
   mounted() {
     let userInfo = localStorage.getItem("user info");
     let parsedUserInfo = JSON.parse(userInfo);
     let userID = parsedUserInfo.id;
     this.employe_id = userID;
+    this.getTotalTime();
     this.getObject();
-
-    setInterval(() => {
-      this.getTotalTime();
-    }, 1000);
   }
 };
 </script>
@@ -216,12 +171,6 @@ export default {
   color: white;
   font-size: 30px;
   font-weight: 1000;
-}
-
-.popup {
-  position: absolute;
-
-  padding: 50px;
 }
 
 .new {
